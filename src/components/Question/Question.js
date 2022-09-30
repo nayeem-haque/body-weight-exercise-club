a3ff5d0eac4386ebaaf13b2aed7f042df7773895
unknown <nayeemhaque43@gmail.com>
2022-09-30 18:29:37 +0600

    update

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -8,7 +8,7 @@ const Question = () => {
         <h3>
           Q1. How does react work?</h3> <br></br>
           <p>
-            Ans: React uses a declarative paradigm that makes it easier to
+            Answer:  React uses a declarative paradigm that makes it easier to
             reason about your application and aims to be both efficient and
             flexible. It designs simple views for each state in your
             application, and React will efficiently update and render just the
@@ -19,7 +19,7 @@ const Question = () => {
         <h3>
           Q2. Differences between props and state? </h3> <br></br> 
           <p>
-            Ans: The main difference between Props are used to pass data from
+             Answer:  The main difference between Props are used to pass data from
             one component to another. The state is a local data storage that is
             local to the component only and cannot be passed to other
             components.
@@ -29,7 +29,7 @@ const Question = () => {
         <h3>
           Q3. What is useEffect work except data load? </h3> <br></br>
           <p>
-            Ans: UseEffect is the solution to many problems: how to fetch data
+            Answer:  UseEffect is the solution to many problems: how to fetch data
             when a component mounts, how to run code when state changes or when
             a prop changes, how to set up timers or intervals, you name it.
             Pretty much anything you want to “do” in a React component other
